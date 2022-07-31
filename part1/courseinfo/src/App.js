1d285d5db5a0d42a0dbb50fc32083d002be8296e
laiyousin <c611611c@gmail.com>
2022-07-31 16:13:19 +0800

    use reduce to compute the sum of array elements

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,21 +1,63 @@
-const Note = ({note}) => {
+const Part = ({part}) => {
   return (
-    <li>{note.content}</li>
+    <>
+      <h3>{part.name} {part.exercises}</h3>
+    </>
   )
 }
 
-const App = ({notes}) => {
+const Content = ({part}) => {
+  return (
+    <>
+      <Part part= {part} />
+    </>
+  )
+}
+
+const Course = ({course}) => {
+  
+  // reduce 
+  // s: previous value, p: current value
+  // , 0: initial value
+  const sum = course.parts.reduce((s, p) => {
+    return s + p.exercises
+  }, 0)
 
   return (
-    <div>
-      <h1>Notes</h1>
-      <ul>
-        {notes.map(note => 
-          <Note key={note.id} note={note} />
-        )}
-      </ul>
-    </div>
+    <>
+      <h1>{course.name}</h1>
+      {course.parts.map(part => 
+        <Content key= {part.id} part= {part} />
+      )}
+      <h3>total of {sum} exercises</h3>
+    </>
   )
+}
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return <Course course={course} />
 }
 
 export default App
