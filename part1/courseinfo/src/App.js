f048b7b7abf0a6ae9839b867d981f13ec18ccf54
laiyousin <c611611c@gmail.com>
2022-07-31 13:44:59 +0800

    Key-attribute

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,95 +1,19 @@
-import { useState } from "react"
-
-const Title = (props) => {
+const Note = ({note}) => {
   return (
-    <>
-      <h1>
-        {props.title}
-      </h1>
-    </>
+    <li>{note.content}</li>
   )
 }
 
-const Button = (props) => {
-  return (
-    <>
-      <button onClick= {props.handleclick}>{props.text}</button>
-    </>
-  )
-}
-
-const StatisticLine = (props) => {
-  return (
-    <tr>
-      <td>{props.text}</td>
-      <td>{props.value}</td> 
-    </tr>
-  )
-}
-const Statistics = (props) => {
-  const sum = props.score[0].number + props.score[1].number + props.score[2].number
-  if( sum <= 0) {
-    return (
-      <>
-        <h3>No feedback given</h3>
-      </>
-    )
-  }
-  else {
-    return (
-      <>
-        <table>
-          <StatisticLine text= {props.score[0].name} value= {props.score[0].number} />
-          <StatisticLine text= {props.score[1].name} value= {props.score[1].number} />
-          <StatisticLine text= {props.score[2].name} value= {props.score[2].number} />
-          <StatisticLine text= 'all' value= {sum} />
-          <StatisticLine text= 'average' value= {(props.score[0].number - props.score[2].number)/sum} />
-          <StatisticLine text= 'positive' value= {props.score[0].number/sum + ' %'} />
-        </table>
-      </>
-    )
-  }
-}
-const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-
-  const score = [
-    {
-      name: 'good',
-      number: good
-    },
-    {
-      name: 'neutral',
-      number: neutral
-    },
-    {
-      name: 'bad',
-      number: bad
-    }
-  ]
-  const handlegoodclick = () => {
-    setGood(pregood => pregood + 1)
-  }
-
-  const handleneutralclick = () => {
-    setNeutral(preneutral => preneutral + 1)
-  }
-
-  const handlegbadclick = () => {
-    setBad(prebad => prebad + 1)
-  }
+const App = ({notes}) => {
 
   return (
     <div>
-      <Title title='give feedback' />
-      <Button text= 'good' handleclick= {handlegoodclick} />
-      <Button text= 'neutral' handleclick= {handleneutralclick} />
-      <Button text= 'bad' handleclick= {handlegbadclick} />
-      <Title title='statics' />
-      <Statistics score= {score}/>
+      <h1>Notes</h1>
+      <ul>
+        {notes.map(note => 
+          <Note key={note.id} note={note} />
+        )}
+      </ul>
     </div>
   )
 }
